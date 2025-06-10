36f30adc463a46578d1af9dabb78a99994e40b42
TODAISY <91897155+todaisy@users.noreply.github.com>
2025-06-10 23:42:18 +0300

    Create text.py

---
+++ bot/text.py
@@ -0,0 +1,11 @@
+greet = "Hello, {name}! I am a bot that can generate meeting minutes and create tasks that were discussed."
+
+greet_0 = ("Привет, {name}! Выбери удобный язык:\n"
+           "\n"
+           "Hello, {name}! Choose your language:")
+greet_ru = "Я бот, который поможет тебе..."
+greet_en = "I am a bot that will help you..."
+
+service_text = "This is a menu where you can connect various services for displaying meeting summaries and recording tasks."
+generate_text = "To start generating a meeting protocol, upload it in a format convenient for you (text, audio, video)."
+
