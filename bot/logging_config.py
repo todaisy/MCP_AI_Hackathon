e2bb3cdd47436309f348e71fc767fe9c01cc8c96
TODAISY <91897155+todaisy@users.noreply.github.com>
2025-06-10 23:47:16 +0300

    Create logging_config.py

---
+++ bot/logging_config.py
@@ -0,0 +1,20 @@
+import logging  
+from config import LOG_FILE
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(os.getenv('LOG_FILE')),
+            logging.StreamHandler()
+        ]
+    )
+
+
+logger = logging.getLogger(__name__)
