1df541b553d84a5cc37c44479784c5201045232f
TODAISY <91897155+todaisy@users.noreply.github.com>
2025-06-14 12:29:24 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,3 +4,8 @@ API_TOKEN = os.getenv("API_TOKEN","")
 ADMIN_ID = os.getenv("ADMIN_ID","")  
 DB_PATH = os.getenv("DB_PATH","")
 LOG_FILE = os.getenv("LOG_FILE","")
+
+TOKEN = os.getenv("TOKEN", "")
+FOLDER = os.getenv("FOLDER", "")
+
+conn = psy.connect()
