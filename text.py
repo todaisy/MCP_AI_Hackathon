cda9d85ead15c975ff7e2f06b74a29e8b875a308
TODAISY <91897155+todaisy@users.noreply.github.com>
2025-06-16 15:55:19 +0300

    Create text.py

---
+++ text.py
@@ -0,0 +1,14 @@
+# кнопки выберите язык ru / eng или по региону как то чекать
+
+greet = ('Здравствуйте, {name}! \nЯ бот, который может генерировать протоколы встреч и выделять задачи, которые обсуждались.\n'
+         'Чтобы перейти к генерации, нажмите кнопку "Generate".')
+
+greet_0 = ("Привет, {name}! Выбери удобный язык:\n"
+           "\n"
+           "Hello, {name}! Choose your language:")
+greet_ru = "Я бот, который поможет тебе..."
+greet_en = "I am a bot that will help you..."
+
+service_text = "This is a menu where you can connect various services for displaying meeting summaries and recording tasks."
+generate_text = "Чтобы приступить к формированию протокола встречи, загрузите прошедшую встречу в удобном для вас формате (текст, word, pdf)."
+
